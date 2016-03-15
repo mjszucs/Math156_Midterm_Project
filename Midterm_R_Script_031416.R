60484ddb207f716390c79a87bd79fcac8ef3ebce
mjszucs <matthew.j.szucs@gmail.com>
2016-03-15 01:06:36 -0400

    New Shit

--- Midterm_R_Script_031416.R
+++ Midterm_R_Script_031416.R
@@ -1,7 +1,60 @@
-#Midterm 
-library(ggplot2)
+#Midterm Project Workspace
 
-setwd(dir = "/Users/Matthew/Documents/School/Mathmatical_Foundations_of_Statistical_Software/Group_Project_Midterm/")
+library(ggplot2)
+setwd(dir = "/Users/mattszucs/Documents/Math156_Midterm_Project/")
+
+#######################
+# This is some preliminary information about what this dataset has and what we can 
+# Do with it
+######################
+
+# Catagorical Variables
+
+# d$State.gov 
+# [1]  Self-emp-not-inc  Private           State-gov         Federal-gov       Local-gov         ?               
+# [7]  Self-emp-inc      Without-pay       Never-worked   
+# 
+# 
+# d$Bachelors
+# [1]  Bachelors     HS-grad       11th          Masters       9th           Some-college  Assoc-acdm    Assoc-voc   
+# [9]  7th-8th       Doctorate     Prof-school   5th-6th       10th          1st-4th       Preschool     12th   
+# 
+# 
+# d$Never.married
+# [1]  Married-civ-spouse     Divorced               Married-spouse-absent  Never-married          Separated            
+# [6]  Married-AF-spouse      Widowed    
+# 
+# unique(d$Adm.clerical)
+# [1]  Exec-managerial    Handlers-cleaners  Prof-specialty     Other-service      Adm-clerical       Sales            
+# [7]  Craft-repair       Transport-moving   Farming-fishing    Machine-op-inspct  Tech-support       ?                
+# [13]  Protective-serv    Armed-Forces       Priv-house-serv  
+
+# > unique(d$White)
+# [1]  White               Black               Asian-Pac-Islander  Amer-Indian-Eskimo  Other 
+
+
+# > unique(d$United.States)
+# [1]  United-States               Cuba                        Jamaica                     India                     
+# [5]  ?                           Mexico                      South                       Puerto-Rico               
+# [9]  Honduras                    England                     Canada                      Germany                   
+# [13]  Iran                        Philippines                 Italy                       Poland                    
+# [17]  Columbia                    Cambodia                    Thailand                    Ecuador                   
+# [21]  Laos                        Taiwan                      Haiti                       Portugal                  
+# [25]  Dominican-Republic          El-Salvador                 France                      Guatemala                 
+# [29]  China                       Japan                       Yugoslavia                  Peru                      
+# [33]  Outlying-US(Guam-USVI-etc)  Scotland                    Trinadad&Tobago             Greece                    
+# [37]  Nicaragua                   Vietnam                     Hong                        Ireland                   
+# [41]  Hungary                     Holand-Netherlands      
+# 
+
+
+
+###########################
+#This is setting up the dataframes 
+#
+#############################
+
+setwd(dir = "/Users/mattszucs/Documents/Math156_Midterm_Project/")
 
 d = read.csv(file = "adult.csv")
 colnames(d) = c("Age","Workclass","Fnlwgt","Education","Education-num","Marital-status",
